e3196d8187b9da594daa85704d3763e33502ba18
Ritwij Sinha <ritwijxsinha@gmail.com>
2018-10-23 15:43:50 +0530

    added input_text and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Btn = styled.button`
+    background: #ffffff;
+    border: none;
+    display: inline-block;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+    // margin: 20px 0;
+    color: #4779E7;
+    padding: 12px 1px;
+    font-size: 1rem;
+    font-weight: bold;
+    width: 20%;
+    border-radius: 30px;
+    outline: none;
+    max-width: calc(800px / 3);
+    cursor: pointer;
+    transition: 0.2s;
+     &:hover {
+        background: #4779E7;
+        color: #fff;
+        transition: 0.2s;
+    }
+`;
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  display: inline-block;
+  color: ${props => props.inputColor || "palevioletred"};
+  border: none;
+  border-radius: 30px;
+  padding: 10px;
+  outline: none;
+  height: 20px;
+  width: 30%;
+`;
+
+
+const BuySellBox = styled.div`
+    padding: 60px 20px;
+    border-radius: 2px;
+    width: 800px;
+    margin: 0 auto;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          
+          <div style={{...({ display: 'inline-block', width: '60%' })}}>
+          <Input type="text"/>
+          <Btn>
+            Search
+          </Btn>
+          </div>
+          
         </header>
       </div>
     );
