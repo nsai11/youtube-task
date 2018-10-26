8f961a9a72e8ad530a72353e76abfbf2ea0784e4
Ritwij Sinha <ritwijxsinha@gmail.com>
2018-10-26 20:14:22 +0530

    displaying results

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import styled from "styled-components";
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 // import SearchBar from 'material-ui-search-bar'
 import 'typeface-roboto';
+import ReactTable from "react-table";
+import 'react-table/react-table.css';
 
 
 const Btn = styled.button`
     background: #ffffff;
     border: none;
     display: inline-block;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
     // margin: 20px 0;
     color: #4779E7;
     padding: 12px 1px;
@@ -33,12 +34,17 @@ const Btn = styled.button`
 const Input = styled.input`
   // padding: 0.5em;
   margin: 1%;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
   display: inline-block;
-  color: ${props => props.inputColor || "palevioletred"};
+  // color: ${props => props.inputColor || "palevioletred"};
+  color: #333;
   border: none;
   border-radius: 30px;
   padding: 10px;
   outline: none;
+  font-size: 1rem;
+  padding-left: 20px;
+  padding-right: 20px;
   height: 20px;
   width: 30%;
 `;
@@ -48,25 +54,33 @@ const API_Key = 'AIzaSyB3iM1KV1Jk_2-iPNXiflbIehCRaxmaBBY';
 
 
 
-const BuySellBox = styled.div`
-    padding: 60px 20px;
-    border-radius: 2px;
-    width: 800px;
-    margin: 0 auto;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
-`;
+// const BuySellBox = styled.div`
+//     padding: 60px 20px;
+//     border-radius: 2px;
+//     width: 800px;
+//     margin: 0 auto;
+//     box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+// `;
 
 var results = {}; 
 var displayResults = [];
-
+const columns = [{
+  Header: 'Title',
+  accessor: 'title'
+}, {
+  Header: 'Channel',
+  accessor: 'channelTitle'
+}, {
+  Header: 'Published At',
+  accessor: 'publishedAt'
+}]
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
       value: '',
-      asc: true,
-      sortby: 0
+      res: [],
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -103,6 +117,7 @@ class App extends Component {
         }
         this.displayTable();
         console.log(displayResults);
+        this.setState({res: displayResults});
       })
       .catch((error) => {
         console.log(error);
@@ -132,7 +147,10 @@ class App extends Component {
         maxWidth: 800
       }}
     /> */}
-          
+          <ReactTable
+            data={this.state.res}
+            columns={columns}
+          />
           
         </header>
       </div>
